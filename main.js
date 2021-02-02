c487bcfbf2d594302860b7515391fe91916a16de
wanglibang <3294713004@qq.com>
2021-02-02 12:29:21 +0800

    Update package.json

---
+++ main.js
@@ -0,0 +1,39 @@
+const {app, BrowserWindow} = require('electron')
+const electron = require('electron')
+const Menu = electron.Menu
+app.on('ready', function createWindow () {
+    //关闭菜单栏
+    Menu.setApplicationMenu(null)
+    // 可以创建多个渲染进程
+    let win = new BrowserWindow({
+        show: false,
+        //resizable: false,
+        icon: __dirname + '/apps/mixly/favicon.ico',
+        allowRunningInsecureContent: true, 
+        webPreferences: {
+          nodeIntegration: true,
+          enableRemoteModule: true
+        }
+    })
+    app.allowRendererProcessReuse = false;
+    //打开开发者工具
+    //win.webContents.openDevTools();
+    win.maximize()
+    win.show()
+
+    // 渲染进程中的web页面可以加载本地文件
+    win.loadFile('index.html')
+
+
+    // 记得在页面被关闭后清除该变量，防止内存泄漏
+    win.on('closed', function () {
+        win = null
+    })
+
+
+})
+
+// 页面全部关闭后关闭主进程,不同平台可能有不同的处理方式
+app.on('window-all-closed', () => {
+    app.quit()
+})
